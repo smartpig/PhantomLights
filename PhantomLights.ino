1f34f21e29082687540fc1b514a46d39002fbade
Lee <lee@cunningnet.com>
2018-10-20 23:42:07 -0400

    shift to new codebase

--- PhantomLights.ino
+++ PhantomLights.ino
@@ -1,37 +1,67 @@
-const int redPin = 11;
-const int greenPin = 10;
-const int bluePin = 9;
+/*
+ * https://github.com/chauhannaman98/ArduinoCodes/tree/master/Using-CC-CA-RGB-LED
+ *
+ * sketch.ino
+ *
+ * July 8, 2018 © GPL3+
+ * Author : Naman Chauhan
+    
+    NOTE: New model RGB leds have green and blue leads interchanged. Please refer     
+    to the datasheet of the correspoding led model from the seller's site.
+    
+    Use the Serial Monitor to enter the values for each color component out of 
+    255.
+    
+***********************************************************************************/
 
-void setup() {
-  // Start off with the LED off.
-  setColourRgb(0,0,0);
-}
+/***declaring RGB pins in PWM mode***/
+int red = 11;
+int green = 9;
+int blue = 10;
 
-void loop() {
-  unsigned int rgbColour[3];
-
-  // Start off with red.
-  rgbColour[0] = 255;
-  rgbColour[1] = 0;
-  rgbColour[2] = 0;
-
-  // Choose the colours to increment and decrement.
-  for (int decColour = 0; decColour < 3; decColour += 1) {
-    int incColour = decColour == 2 ? 0 : decColour + 1;
-
-    // cross-fade the two colours.
-    for(int i = 0; i < 255; i += 1) {
-      rgbColour[decColour] -= 1;
-      rgbColour[incColour] += 1;
-
-      setColourRgb(rgbColour[0], rgbColour[1], rgbColour[2]);
-      delay(5);
-    }
+/***brightness control variables***/
+int redBrightness = 0;
+int greenBrightness = 0;
+int blueBrightness = 0;
+  
+  void setup() {
+  
+    Serial.begin(9600);         //initialising serial monitor
+  
+    /***Pin mode declaration***/
+    pinMode(red, OUTPUT);
+    pinMode(green, OUTPUT);
+    pinMode(blue, OUTPUT);
+    
   }
-}
-
-void setColourRgb(unsigned int red, unsigned int green, unsigned int blue) {
-  analogWrite(redPin, red);
-  analogWrite(greenPin, green);
-  analogWrite(bluePin, blue);
- }
+  
+  void loop() {
+    
+    Serial.println("::Enter color brightness::");
+    
+    Serial.print("Red : ");         //for red
+    while(Serial.available()==0){
+    }
+    redBrightness = 255 - Serial.parseInt();
+    Serial.println(redBrightness);
+    
+    Serial.print("Green : ");         //for green
+    while(Serial.available()==0){
+    }
+    greenBrightness = 255 - Serial.parseInt();
+    Serial.println(greenBrightness);
+  
+    Serial.print("Blue : ");         //for blue
+    while(Serial.available()==0){
+    }
+    blueBrightness = 255 - Serial.parseInt();
+    Serial.println(blueBrightness);
+    Serial.println("   ");
+  
+    /***sending variable voltages to RGB led***/
+    analogWrite(red, redBrightness);
+    analogWrite(green, greenBrightness);
+    analogWrite(blue, blueBrightness);
+    delay(3000);
+    
+    }
